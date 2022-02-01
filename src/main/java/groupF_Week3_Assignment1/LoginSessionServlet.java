e5de58bca5e347c9d116bb9da419431d2ac78dc2
Monica <Monica@192.168.2.104>
2022-01-31 23:43:10 -0500

    SessionServletChanges

--- src/main/java/groupF_Week3_Assignment1/LoginSessionServlet.java
+++ src/main/java/groupF_Week3_Assignment1/LoginSessionServlet.java
@@ -22,47 +22,51 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/LoginSessionServlet")
 public class LoginSessionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginSessionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LoginSessionServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		try{  
-			  
-	        response.setContentType("text/html");  
-	        PrintWriter out = response.getWriter();  
-	          
-	        String n=request.getParameter("username");  
-	        out.print("Welcome "+n);  
-	          
-	        HttpSession session=request.getSession();  
-	        session.setAttribute("uname",n);  
-	  
-	        out.print("<br/><a href='LoginSessionsServlet1'>visit</a>");  
-	                  
-	        out.close();  
-	  
-	        }
-		
-		catch(Exception e){System.out.println(e);}  
-	    } 
+		try {
+
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+
+			String n = request.getParameter("username");
+			out.print("Welcome " + n);
+
+			HttpSession session = request.getSession();
+			session.setAttribute("uname", n);
+
+			out.print("<br/><a href='LoginSessionsServlet1'>visit</a>");
+
+			out.close();
+
+		}
+
+		catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-
-
+}
